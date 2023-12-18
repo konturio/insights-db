af4ab565e70acfa061d8652aff3d3b0804724d49
sleeping <hh.sleeping@gmail.com>
2023-12-18 19:20:42 +0400

    rename param_uuid -> internal_id

--- scripts/remove_outated_indicators.sql
+++ scripts/remove_outated_indicators.sql
@@ -1,6 +1,6 @@
 delete from stat_h3_transposed t
 using bivariate_indicators_metadata b
-where t.indicator_uuid = b.param_uuid and state = 'OUTDATED';
+where t.indicator_uuid = b.internal_id and state = 'OUTDATED';
 
 delete from bivariate_indicators_metadata
 where state = 'OUTDATED';
