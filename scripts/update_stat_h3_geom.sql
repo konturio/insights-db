2a7ae6fdb0d3d26c7dc4eb80e7c85d73cfcd3682
sleeping <hh.sleeping@gmail.com>
2024-03-26 15:10:23 +0400

    update_stat_h3_geom: filter by recent indicators

--- scripts/update_stat_h3_geom.sql
+++ scripts/update_stat_h3_geom.sql
@@ -2,5 +2,9 @@ INSERT INTO stat_h3_geom (h3, resolution, geom)
 SELECT t.h3,
        h3_get_resolution(t.h3),
        ST_Transform(h3_cell_to_boundary_geometry(t.h3), 3857) as geom
-FROM (SELECT DISTINCT h3 FROM stat_h3_transposed order by h3) t
+FROM (
+    SELECT DISTINCT h3 FROM stat_h3_transposed
+    where indicator_uuid in (select internal_id from bivariate_indicators_metadata where date > now() - interval '2 days')
+    order by h3
+) t
 WHERE NOT exists (SELECT FROM stat_h3_geom g WHERE g.h3 = t.h3)
