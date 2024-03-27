e55c076bc2d942162e32f28797330eadec96a4b9
sleeping <hh.sleeping@gmail.com>
2024-03-27 17:32:06 +0400

    remove temp view from procedures/16269_bivariate_axis_analytics.sql

--- procedures/16269_bivariate_axis_analytics.sql
+++ procedures/16269_bivariate_axis_analytics.sql
@@ -6,11 +6,12 @@ as
 $$
 declare
     skip_stops boolean := false;
-    percentiles_keys text[] := '{p33, p50, p66}';
-    percentiles float[] := '{.33, .5, .66}';
+    percentiles_keys text := '{p33, p50, p66}';
+    percentiles text := '{.33, .5, .66}';
     area_km2_uuid uuid;
     one_uuid uuid;
     den_value text;
+    tmp_axis_sql text;
 begin
     select internal_id into area_km2_uuid from bivariate_indicators_metadata
     where owner = 'insights-db' and param_id = 'area_km2';
@@ -44,15 +45,14 @@ begin
             den_value := '1.';
         end case;
 
-        execute 'create temp view tmp_axis as
-        select h3, indicator_value / '|| den_value ||' m
-        from stat_h3_transposed
+        tmp_axis_sql := 'stat_h3_transposed,
+        lateral (
+            select indicator_value / '|| den_value ||' as m
+        ) z
         where indicator_uuid = ' || quote_literal(x_numerator_uuid);
     else
         -- common case: select both indicators from db
-        execute 'create temp view tmp_axis as
-        select h3, z.m
-        from (
+        tmp_axis_sql := '(
             select h3, indicator_value
             from stat_h3_transposed
             where indicator_uuid = '|| quote_literal(x_numerator_uuid) ||'
@@ -67,20 +67,20 @@ begin
         ) z';
     end if;
 
-    with statistics as (select h3_get_resolution(h3) as r,
+    execute 'with statistics as (select h3_get_resolution(h3) as r,
                                jsonb_build_object(
-                                       'sum', nullif(sum(m), 0),
-                                       'min', min(m) filter (where m != 0),
-                                       'max', max(m) filter (where m != 0),
-                                       'mean', nullif(avg(m), 0),
-                                       'stddev', nullif(stddev(m), 0)
+                                       ''sum'', nullif(sum(m), 0),
+                                       ''min'', min(m) filter (where m != 0),
+                                       ''max'', max(m) filter (where m != 0),
+                                       ''mean'', nullif(avg(m), 0),
+                                       ''stddev'', nullif(stddev(m), 0)
                                    )
                                 || 
                                 jsonb_object(
-                                    percentiles_keys,
-                                    (percentile_cont(percentiles) within group (order by m))::text[]
+                                    '|| quote_literal(percentiles_keys) ||',
+                                    (percentile_cont('|| quote_literal(percentiles) ||'::float[]) within group (order by m))::text[]
                                 ) as stats
-                        from tmp_axis
+                        from '|| tmp_axis_sql ||'
                         group by r
                         order by r),
          quality as (select key,
@@ -100,25 +100,27 @@ begin
          upd as (select jsonb_object_agg(key, array [value, quality]) j
                  from quality)
     update bivariate_axis_v2 ba
-    set sum_value = (j -> 'sum' ->> 0)::double precision,
-        sum_quality = (j -> 'sum' ->> 1)::double precision,
-        min_value      = (j -> 'min' ->> 0)::double precision,
-        min_quality    = (j -> 'min' ->> 1)::double precision,
-        max_value      = (j -> 'max' ->> 0)::double precision,
-        max_quality    = (j -> 'max' ->> 1)::double precision,
-        stddev_value   = (j -> 'stddev' ->> 0)::double precision,
-        stddev_quality = (j -> 'stddev' ->> 1)::double precision,
-        median_value   = (j -> 'p50' ->> 0)::double precision,
-        median_quality = (j -> 'p50' ->> 1)::double precision,
-        mean_value     = (j -> 'mean' ->> 0)::double precision,
-        mean_quality   = (j -> 'mean' ->> 1)::double precision,
-        -- stops:
-        min = case when skip_stops then min else floor((j -> 'min' ->> 0)::double precision) end,
-        p25 = case when skip_stops then p25 else (j -> 'p33' ->> 0)::double precision end,
-        p75 = case when skip_stops then p75 else (j -> 'p66' ->> 0)::double precision end,
-        max = case when skip_stops then max else ceil((j -> 'max' ->> 0)::double precision) end
-    from upd
-    where ba.numerator_uuid = x_numerator_uuid
-      and ba.denominator_uuid = x_denominator_uuid;
+    set sum_value = (j -> ''sum'' ->> 0)::double precision,
+        sum_quality = (j -> ''sum'' ->> 1)::double precision,
+        min_value      = (j -> ''min'' ->> 0)::double precision,
+        min_quality    = (j -> ''min'' ->> 1)::double precision,
+        max_value      = (j -> ''max'' ->> 0)::double precision,
+        max_quality    = (j -> ''max'' ->> 1)::double precision,
+        stddev_value   = (j -> ''stddev'' ->> 0)::double precision,
+        stddev_quality = (j -> ''stddev'' ->> 1)::double precision,
+        median_value   = (j -> ''p50'' ->> 0)::double precision,
+        median_quality = (j -> ''p50'' ->> 1)::double precision,
+        mean_value     = (j -> ''mean'' ->> 0)::double precision,
+        mean_quality   = (j -> ''mean'' ->> 1)::double precision' ||
+            case when skip_stops then ''
+            else ',
+        min = floor((j -> ''min'' ->> 0)::double precision),
+        p25 = (j -> ''p33'' ->> 0)::double precision,
+        p75 = (j -> ''p66'' ->> 0)::double precision,
+        max = ceil((j -> ''max'' ->> 0)::double precision) '
+            end ||
+    'from upd
+    where ba.numerator_uuid = '|| quote_literal(x_numerator_uuid) ||'
+      and ba.denominator_uuid = '|| quote_literal(x_denominator_uuid);
 end;
 $$;
--- test/analytics.sql
+++ test/analytics.sql
@@ -5,55 +5,63 @@
 --
 -- case when denominator is 'one'
 --
-create temp view tmp_axis as
-select h3, indicator_value / 1. m
-from stat_h3_transposed
-where indicator_uuid = :x_numerator_uuid;
-
 with statistics as (select h3_get_resolution(h3) as r,
-                           jsonb_build_object(
-                                   'sum', nullif(sum(m), 0),
-                                   'min', min(m) filter (where m != 0),
-                                   'max', max(m) filter (where m != 0),
-                                   'mean', nullif(avg(m), 0),
-                                   'stddev', nullif(stddev(m), 0)
-                               )
-                            ||
-                            jsonb_object(
-                                '{p33, p50, p66}',
-                                (percentile_cont('{.33, .5, .66}'::float[]) within group (order by m))::text[]
-                            ) as stats
-                    from tmp_axis
-                    group by r
-                    order by r),
-     quality as (select key,
-                        avg(value) filter (where r = 8) as value,
-                        case
-                            when avg(value) filter (where r = 8) is null
-                                then null
-                            when (nullif(max(value), 0) / nullif(min(value), 0)) > 0
-                                then log10(nullif(max(value), 0) / nullif(min(value), 0))
-                            else log10((nullif(max(value), 0) - nullif(min(value), 0)) /
-                                       least(abs(nullif(min(value), 0)), abs(nullif(max(value), 0))))
-                            end                            quality
-                 from statistics x,
-                      jsonb_object_keys(stats) key,
-                      lateral (values ((stats ->> key)::double precision)) as v(value)
-                 group by key),
-     upd as (select jsonb_object_agg(key, array [value, quality]) j
+                       jsonb_build_object(
+                               'sum', nullif(sum(m), 0),
+                               'min', min(m) filter (where m != 0),
+                               'max', max(m) filter (where m != 0),
+                               'mean', nullif(avg(m), 0),
+                               'stddev', nullif(stddev(m), 0)
+                           )
+                        || 
+                        jsonb_object(
+                            '{p33, p50, p66}',
+                            (percentile_cont('{.33, .5, .66}'::float[]) within group (order by m))::text[]
+                        ) as stats
+                from stat_h3_transposed,
+lateral (
+    select indicator_value / 1. as m
+) z
+where indicator_uuid = :x_numerator_uuid
+                group by r
+                order by r),
+ quality as (select key,
+                    avg(value) filter (where r = 8) as value,
+                    case
+                        when avg(value) filter (where r = 8) is null
+                            then null
+                        when (nullif(max(value), 0) / nullif(min(value), 0)) > 0
+                            then log10(nullif(max(value), 0) / nullif(min(value), 0))
+                        else log10((nullif(max(value), 0) - nullif(min(value), 0)) /
+                                   least(abs(nullif(min(value), 0)), abs(nullif(max(value), 0))))
+                        end                            quality
+             from statistics x,
+                  jsonb_object_keys(stats) key,
+                  lateral (values ((stats ->> key)::double precision)) as v(value)
+             group by key),
+ upd as (select jsonb_object_agg(key, array [value, quality]) j
              from quality)
 select :x_numerator_uuid num, 'one' den, jsonb_pretty(j) stats
 from upd;
-
-drop view tmp_axis;
 
 --
 -- common case: both indicators are from user
 --
 
-create temp view tmp_axis as
-select h3, z.m
-from (
+with statistics as (select h3_get_resolution(h3) as r,
+                       jsonb_build_object(
+                               'sum', nullif(sum(m), 0),
+                               'min', min(m) filter (where m != 0),
+                               'max', max(m) filter (where m != 0),
+                               'mean', nullif(avg(m), 0),
+                               'stddev', nullif(stddev(m), 0)
+                           )
+                        ||
+                        jsonb_object(
+                            '{p33, p50, p66}',
+                            (percentile_cont('{.33, .5, .66}'::float[]) within group (order by m))::text[]
+                        ) as stats
+                from (
     select h3, indicator_value
     from stat_h3_transposed
     where indicator_uuid = :x_numerator_uuid
@@ -65,41 +73,25 @@ join (
     order by indicator_uuid, h3) denominator using(h3),
 lateral (
     select numerator.indicator_value / nullif(denominator.indicator_value, 0) as m
-) z;
+) z
+                group by r
+                order by r),
+quality as (select key,
+                   avg(value) filter (where r = 8) as value,
+                   case
+                       when avg(value) filter (where r = 8) is null
+                           then null
+                       when (nullif(max(value), 0) / nullif(min(value), 0)) > 0
+                           then log10(nullif(max(value), 0) / nullif(min(value), 0))
+                       else log10((nullif(max(value), 0) - nullif(min(value), 0)) /
+                                  least(abs(nullif(min(value), 0)), abs(nullif(max(value), 0))))
+                       end                            quality
+            from statistics x,
+                 jsonb_object_keys(stats) key,
+                 lateral (values ((stats ->> key)::double precision)) as v(value)
+            group by key),
+upd as (select jsonb_object_agg(key, array [value, quality]) j
+        from quality)
 
-with statistics as (select h3_get_resolution(h3) as r,
-                           jsonb_build_object(
-                                   'sum', nullif(sum(m), 0),
-                                   'min', min(m) filter (where m != 0),
-                                   'max', max(m) filter (where m != 0),
-                                   'mean', nullif(avg(m), 0),
-                                   'stddev', nullif(stddev(m), 0)
-                               )
-                            ||
-                            jsonb_object(
-                                '{p33, p50, p66}',
-                                (percentile_cont('{.33, .5, .66}'::float[]) within group (order by m))::text[]
-                            ) as stats
-                    from tmp_axis
-                    group by r
-                    order by r),
-     quality as (select key,
-                        avg(value) filter (where r = 8) as value,
-                        case
-                            when avg(value) filter (where r = 8) is null
-                                then null
-                            when (nullif(max(value), 0) / nullif(min(value), 0)) > 0
-                                then log10(nullif(max(value), 0) / nullif(min(value), 0))
-                            else log10((nullif(max(value), 0) - nullif(min(value), 0)) /
-                                       least(abs(nullif(min(value), 0)), abs(nullif(max(value), 0))))
-                            end                            quality
-                 from statistics x,
-                      jsonb_object_keys(stats) key,
-                      lateral (values ((stats ->> key)::double precision)) as v(value)
-                 group by key),
-     upd as (select jsonb_object_agg(key, array [value, quality]) j
-             from quality)
 select :x_numerator_uuid num, :x_denominator_uuid den, jsonb_pretty(j) stats
 from upd;
-
-drop view tmp_axis;
