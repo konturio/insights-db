0f8f3ca619d9a22e6d36960ac380f8e1a8a5c814
sleeping <hh.sleeping@gmail.com>
2024-04-22 20:21:54 +0400

    calculate stops for all resolutions, consider population and exact h3 area

--- procedures/16269_bivariate_axis_analytics.sql
+++ procedures/16269_bivariate_axis_analytics.sql
@@ -10,7 +10,8 @@ declare
     percentiles text := '{.33, .5, .66}';
     area_km2_uuid uuid;
     one_uuid uuid;
-    den_value text;
+    population_uuid uuid;
+    population_sql text := '';
     tmp_axis_sql text;
 begin
     select internal_id into area_km2_uuid from bivariate_indicators_metadata
@@ -18,6 +19,9 @@ begin
 
     select internal_id into one_uuid from bivariate_indicators_metadata
     where owner = 'disaster.ninja' and param_id = 'one';
+
+    select internal_id into population_uuid from bivariate_indicators_metadata
+    where owner = 'disaster.ninja' and param_id = 'population' order by date desc limit 1;
 
     if x_numerator_uuid in (area_km2_uuid, one_uuid) then
         -- we don't need such axis
@@ -37,21 +41,17 @@ begin
         percentiles = '{.5}';
     end if;
 
-    if x_denominator_uuid in (area_km2_uuid, one_uuid) then
-        case x_denominator_uuid
-        when area_km2_uuid then
-            -- TODO should not use approx area for this task, but h3_cell_area is toooo slow
-            -- https://kontur.fibery.io/Tasks/Task/fix-area-calculation-for-analytics-task-18169
-            den_value := 'h3_get_hexagon_area_avg(h3_get_resolution(h3))';
-        when one_uuid then
-            den_value := '1.';
-        end case;
+    if not skip_stops and population_uuid != x_numerator_uuid then
+        population_sql = ' left join (
+            select h3, indicator_value p
+            from stat_h3_transposed
+            where indicator_uuid = '|| quote_literal(population_uuid) ||'
+            order by indicator_uuid, h3) w using(h3)';
+    end if;
 
-        tmp_axis_sql := 'stat_h3_transposed,
-        lateral (
-            select indicator_value / '|| den_value ||' as m
-        ) z
-        where indicator_uuid = ' || quote_literal(x_numerator_uuid);
+    if x_denominator_uuid = one_uuid then
+        tmp_axis_sql := '(select h3, indicator_value m from stat_h3_transposed
+        where indicator_uuid = ' || quote_literal(x_numerator_uuid) || ')' || population_sql;
     else
         -- common case: select both indicators from db
         tmp_axis_sql := '(
@@ -63,7 +63,8 @@ begin
             select h3, indicator_value
             from stat_h3_transposed
             where indicator_uuid = '|| quote_literal(x_denominator_uuid) ||'
-            order by indicator_uuid, h3) denominator using(h3),
+            order by indicator_uuid, h3) denominator using(h3)
+        ' || population_sql || ',
         lateral (
             select numerator.indicator_value / nullif(denominator.indicator_value, 0) as m
         ) z';
@@ -80,11 +81,13 @@ begin
                                 || 
                                 jsonb_object(
                                     '|| quote_literal(percentiles_keys) ||',
-                                    (percentile_cont('|| quote_literal(percentiles) ||'::float[]) within group (order by m))::text[]
+                                    (percentile_cont('|| quote_literal(percentiles) ||'::float[]) within group (order by m)' ||
+                                        case when skip_stops or population_uuid = x_numerator_uuid then '' else ' filter (where p is not null) ' end || ')::text[]
                                 ) as stats
                         from '|| tmp_axis_sql ||'
-                        group by r
+                        group by rollup (r)
                         order by r),
+         stops as (select stats s from statistics where r is null),
          quality as (select key,
                             avg(value) filter (where r = 8) as value,
                             case
@@ -113,15 +116,15 @@ begin
         median_value   = (j -> ''p50'' ->> 0)::double precision,
         median_quality = (j -> ''p50'' ->> 1)::double precision,
         mean_value     = (j -> ''mean'' ->> 0)::double precision,
-        mean_quality   = (j -> ''mean'' ->> 1)::double precision' ||
+        mean_quality   = (j -> ''mean'' ->> 1)::double precision ' ||
             case when skip_stops then ''
             else ',
-        min = floor((j -> ''min'' ->> 0)::double precision),
-        p25 = (j -> ''p33'' ->> 0)::double precision,
-        p75 = (j -> ''p66'' ->> 0)::double precision,
-        max = ceil((j -> ''max'' ->> 0)::double precision) '
+        min = floor((stops.s ->> ''min'')::double precision),
+        p25 = (stops.s ->> ''p33'')::double precision,
+        p75 = (stops.s ->> ''p66'')::double precision,
+        max = ceil((stops.s ->> ''max'')::double precision) '
             end ||
-    'from upd
+    'from upd, stops
     where ba.numerator_uuid = '|| quote_literal(x_numerator_uuid) ||'
       and ba.denominator_uuid = '|| quote_literal(x_denominator_uuid);
 end;
