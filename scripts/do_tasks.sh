293410e5e23237ca04a39deab686bf0529d4b88c
sleeping <hh.sleeping@gmail.com>
2025-03-03 17:40:43 +0400

    parametrize statement_timeout

--- scripts/do_tasks.sh
+++ scripts/do_tasks.sh
@@ -9,6 +9,6 @@ while true; do
         sleep 120
         continue
     fi
-    seq `psql -c 'select count(0) from task_queue' -t` | parallel -j ${MAX_PARALLEL_TASKS:=3} -n0 'psql -q -c "set statement_timeout='\''1h'\''" -c "call dispatch()"'
+    seq `psql -c 'select count(0) from task_queue' -t` | parallel -j ${MAX_PARALLEL_TASKS:=3} -n0 "psql -q -c 'set statement_timeout=\"${TASK_TIMEOUT:-1h}\"' -c 'call dispatch()'"
     sleep 10
 done
