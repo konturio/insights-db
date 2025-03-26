6c129105303b10664a88972a67c63b45a0d3eb3c
avalasiuk <avalasiuk@kontur.io>
2025-03-26 20:01:06 +0100

    Update psql request to run analyze only if needed

--- scripts/do_tasks.sh
+++ scripts/do_tasks.sh
@@ -9,9 +9,12 @@ while true; do
         sleep 120
         continue
     fi
-    copy_task_exists=$(psql -t -c "select 1 from task_queue where task_type='copy' limit 1")
+
+    copy_task_exists=$(psql -tAXc "select 1 from task_queue where task_type='copy' limit 1")
+
     seq `psql -c 'select count(0) from task_queue' -t` | parallel -j ${MAX_PARALLEL_TASKS:=3} -n0 "psql -q -c 'set statement_timeout=\"${TASK_TIMEOUT:-1h}\"' -c 'call dispatch()'"
-    if [ -n "$copy_task_exists" ]; then
+
+    if [ "$copy_task_exists" = "1" ]; then
         vacuumdb -j 300 --analyze-only
     fi
     sleep 10
