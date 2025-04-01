6bde6dbf15a604fdc05c96c52c4799a045f8fb55
sleeping <hh.sleeping@gmail.com>
2025-04-01 11:44:20 +0400

    select partitions to run analyze on

--- scripts/do_tasks.sh
+++ scripts/do_tasks.sh
@@ -10,12 +10,19 @@ while true; do
         continue
     fi
 
-    copy_task_exists=$(psql -tAXc "select 1 from task_queue where task_type='copy' limit 1")
+    tasks_before=`psql -tAXc "select array_agg(x) from (select x_numerator_id x from task_queue where task_type='copy')"`
 
     seq `psql -c 'select count(0) from task_queue' -t` | parallel -j ${MAX_PARALLEL_TASKS:=3} -n0 "psql -q -c 'set statement_timeout=\"${TASK_TIMEOUT:-1h}\"' -c 'call dispatch()'"
 
-    if [ "$copy_task_exists" = "1" ]; then
-        vacuumdb -j 300 --analyze-only
+    tasks_after=`psql -tAXc "select array_agg(x) from (select x_numerator_id x from task_queue where task_type='copy')"`
+
+    completed=`psql -tAXc "with before(x) as (select unnest('${tasks_before:-{\}}'::uuid[])), after(x) as (select unnest('${tasks_after:-{\}}'::uuid[])), completed(x) as (select x from before except select x from after) select array_agg(x) from completed"`
+
+    if [ -n "$completed" ]; then
+        partitions=`psql -tAXc "select distinct 'stat_h3_transposed_p'||i from generate_series(0,255) i, unnest('${completed:-{\}}'::uuid[]) m where satisfies_hash_partition((select oid from pg_class where relname = 'stat_h3_transposed'), 256, i, m)"`
+        command="vacuumdb -v -j 300 --analyze-only $(for p in $partitions; do echo -n ' -t ' $p; done)"
+        echo running $command
+        eval $command
     fi
     sleep 10
 done
